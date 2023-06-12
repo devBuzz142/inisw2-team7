30a76504d1b8c520c8006cdddf47754242eb5818
devBuzz142 <devbuzz142@gmail.com>
2023-06-12 16:18:28 +0900

    feat: frame 기초 구현

--- fe/src/pages/EditPage.jsx
+++ fe/src/pages/EditPage.jsx
@@ -1,18 +1,20 @@
+import Frame from "../components/Frame";
+import { useState } from "react";
+
 const EditPage = () => {
+  const [selected, setSeletced] = useState(0);
+
+  const handleSelect = (index) => setSeletced(index);
+
   return (
     <div>
       <div style={{ width: 720, height: 480, border: "1px solid white" }}>
-        <img src="/src/assets/sample01.jpg" />
+        <img
+          src={`/src/assets/pyframes/${String(selected).padStart(6, "0")}.jpg`}
+        />
       </div>
-      <div style={{ display: "flex" }}>
-        <div className="frame">image01</div>
-        <div className="frame">image02</div>
-        <div className="frame">image03</div>
-        <div className="frame">image04</div>
-        <div className="frame">image05</div>
-        <div className="frame">image06</div>
-        <div className="frame">image07</div>
-      </div>
+      <div>{selected} / 503</div>
+      <Frame length={503} handleSelect={handleSelect} />
       <div style={{ marginTop: 16 }}>
         <button>Restore</button>
         <button>Edit</button>
