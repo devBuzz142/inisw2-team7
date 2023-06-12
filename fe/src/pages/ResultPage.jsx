47bafb3cc582153e60c6abfc5c390d4d70d1e11a
devBuzz142 <devbuzz142@gmail.com>
2023-06-12 21:31:00 +0900

    feat: init

--- fe/src/pages/ResultPage.jsx
+++ fe/src/pages/ResultPage.jsx
@@ -1,5 +1,25 @@
+import { useRef, useState } from "react";
+
 const ResultPage = () => {
-  return <div>hello result</div>;
+  const videoRef = useRef(null);
+  const [videoUrl, setVideoUrl] = useState("");
+
+  return (
+    <div>
+      <button>
+        <h2 style={{ marginBottom: 4, marginTop: 4 }}>WhoSpeak</h2>
+      </button>
+      {videoUrl && (
+        <div style={{ marginTop: "20px" }}>
+          <video width="320" height="240" controls ref={videoRef}>
+            <source src={videoUrl} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      )}
+      <button>Download</button>
+    </div>
+  );
 };
 
 export default ResultPage;
