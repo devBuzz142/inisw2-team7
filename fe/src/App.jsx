07bd744a3100c6cee172d6d06979b30c54a17806
devBuzz142 <devbuzz142@gmail.com>
2023-06-18 01:58:30 +0900

    feat: context provider applying...

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,20 +2,23 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EditPage from "./pages/EditPage";
 import ResultPage from "./pages/ResultPage";
+import SubtitleProvider from "./context/SubtitleProvider";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/edit" element={<EditPage />} />
-            <Route path="/result" element={<ResultPage />} />
-            <Route path="/*" element={<Navigate replace to="/home" />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <SubtitleProvider>
+        <div className="App">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/edit" element={<EditPage />} />
+              <Route path="/result" element={<ResultPage />} />
+              <Route path="/*" element={<Navigate replace to="/home" />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </SubtitleProvider>
     </>
   );
 }
--- fe/src/context/SubtitleProvider.jsx
+++ fe/src/context/SubtitleProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer, useEffect } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const SubtitleContext = createContext();
 export const useSubtitleContext = () => useContext(SubtitleContext);
