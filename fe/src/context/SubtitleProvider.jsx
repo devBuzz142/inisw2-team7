02e0da662d829ca2c4251fb41f6888f53f8e0fea
devBuzz142 <devbuzz142@gmail.com>
2023-06-18 01:45:14 +0900

    feat: context provider init

---
+++ fe/src/context/SubtitleProvider.jsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useReducer, useEffect } from "react";
+
+const SubtitleContext = createContext();
+export const useSubtitleContext = () => useContext(SubtitleContext);
+
+const initialState = {
+  selected: { frame: 1, scene: 1 },
+  subtitles: [],
+  frameCount: 0,
+};
+
+const subtitleReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_SELECTED":
+      return {
+        ...state,
+        selected: action.payload,
+      };
+    case "SET_SUBTITLES":
+      return {
+        ...state,
+        subtitles: action.payload,
+      };
+    case "SET_FRAME_COUNT":
+      return {
+        ...state,
+        frameCount: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const SubtitleProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(subtitleReducer, initialState);
+
+  return (
+    <SubtitleContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}>
+      {children}
+    </SubtitleContext.Provider>
+  );
+};
+
+export default SubtitleProvider;
