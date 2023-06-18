704770ccdade36882e9683d71e743bcdb122d840
devBuzz142 <devbuzz142@gmail.com>
2023-06-19 03:24:32 +0900

    style: logo border

--- fe/src/components/Logo/index.jsx
+++ fe/src/components/Logo/index.jsx
@@ -4,7 +4,7 @@ const Logo = ({ label = "WhoSpeak" }) => {
   const navigate = useNavigate();
 
   return (
-    <button onClick={() => navigate("/")}>
+    <button className="hover-button" onClick={() => navigate("/")}>
       <h2 style={{ marginBottom: 4, marginTop: 4 }}>{label}</h2>
     </button>
   );
